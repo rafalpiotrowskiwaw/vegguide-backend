c4ca595734e0fb3d57eae5774e600d2332ab0650
Rafał Piotrowski <rafal.piotrowski@mindmade.pl>
2022-07-27 08:32:55 +0200

    ok

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 const bodyParser = require("body-parser");
 
 import router from './router'
 
 const app: Express = express();
 const port = '8080';
+
+app.use(cors())
 app.use(express.json())
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use('/', router);
 
